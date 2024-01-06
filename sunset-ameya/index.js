2a433122d52f66165280a2a8984e7a10abe4e5fc
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-01-06 11:39:46 -0500

    finished sunset image

--- sunset-ameya/index.js
+++ sunset-ameya/index.js
@@ -5,6 +5,11 @@ const HEIGHT = 125;
 const WINDOWSIZE = 3; // width and height of building windows
 const WINDOWGAP = WINDOWSIZE / 3
 const HORIZON = 45; // holds the y-val of the horizon
+const SUNRAD = 36;
+const RAYNUM = 12
+
+const leftArcEnd = (WIDTH - (SUNRAD * 2)) / 2;
+const rightArcEnd = leftArcEnd + (SUNRAD * 2);
 
 // functions
 
@@ -21,7 +26,7 @@ function SDFCircle(pt, x, y, radius) {
   return ptVal
 }
 
-function SDFRect(pt, x, y, width, height) {
+function SDFRect(pt, x, y, width, height) {// basic implementation
   let ptVal = 0;
   
   const ptX = pt[0];
@@ -79,22 +84,24 @@ function SDFRect(pt, x, y, width, height) {
   return ptVal;
 }
 
-function pointCoord(angle, radius) { // returns the coords of a point provided a radius and an angle to subtend around a circle of that radius
+function pointCoord(angle, radius, x = 0, y = 0) { // returns the coords of a point provided a radius and an angle to subtend around a circle of that radius
   return [
-    (Math.cos(angle) * radius) - radius,
-    Math.sin(angle) * radius
+    (Math.cos(angle) * radius) - radius + x,
+    (Math.sin(angle) * radius) + y
   ];
 };
 
-function arc(angle, radius, points = 250) { // arc generation but good and precise
+function arc(angle, radius, x, y, points = 250) { // arc generation but good and precise
   const arc = createTurtle();
   const angleRad = angle * (Math.PI / 180);
+
+  arc.jump([x,y]);
 
   let arcAngle = 0;
 
   for (let i = 1; i <= points; i++) {
     arcAngle += angleRad / points;    
-    arc.goTo(pointCoord(arcAngle, radius));
+    arc.goTo(pointCoord(arcAngle, radius, x, y));
   };
   
   arc.translate(
@@ -144,6 +151,14 @@ function building(x, y, width, height) {
   return tempBuilding
 }
 
+// sun gen
+
+let sun = createTurtle();
+
+sun.join(arc(220, SUNRAD, 0, 10));
+
+sun.rotate(-20, [WIDTH / 2, HEIGHT / 2])
+
 // building gen
 
 let buildings = createTurtle();
@@ -156,40 +171,89 @@ for (let i = 0; i < 2; i++) {
   let buildingsWidth = 0;
 
   while (buildingsWidth < maxWidth) {
-    let tempWidth = randInRange(6, 14);
-    let tempHeight = randInRange(20, 49);
+    let tempWidth = randIntInRange(6, 14);
+    let tempHeight = randIntInRange(20, 49);
 
     if ((buildingsWidth + tempWidth) > maxWidth) {
       break;
     }
-  
+
+    if (leftArcEnd > x && leftArcEnd < (x + tempWidth)) {
+      sun.iteratePath(pt => {
+        let [x, y] = pt;
+        if (x < (WIDTH / 2) && y < (HORIZON + tempHeight)) {
+          return "REMOVE";
+        }
+        return [x, y]
+      })
+    }
+
+    if (rightArcEnd > x && rightArcEnd < (x + tempWidth)) {
+      sun.iteratePath(pt => {
+        let [x, y] = pt;
+        if (x > (WIDTH / 2) && y < (HORIZON + tempHeight)) {
+          return "REMOVE";
+        }
+        return [x, y]
+      })
+    }
+    
     let tempBuilding = building(x, HORIZON, tempWidth, tempHeight);
     buildings.join(tempBuilding);
 
     x += tempWidth;
     buildingsWidth += tempWidth
 
-    sdfParams += [x, HORIZON, tempWidth, tempHeight];
+    // sdfParams.push([x, HORIZON, tempWidth, tempHeight]);
   }
 
   if (buildingsWidth != maxWidth) {
     const leftoverWidth = maxWidth - buildingsWidth;
-    const tempHeight = randInRange(20, 49);
-    const tempBuilding = building(x, HORIZON, leftoverWidth, randInRange(20,49));
+    const tempHeight = randIntInRange(20, 49);
+    const tempBuilding = building(x, HORIZON, leftoverWidth, randIntInRange(20,49));
     buildings.join(tempBuilding);
   }
 }
 
+
+// sun.resample(0.01);
+
+// ray gen
+
+// for (let i = 0; i < RAYNUM; i++) {
+//   let angle = i * (180 / RAYNUM);
+//   let draw = true; // used to tell whether to draw or whether to skip
+//   let ray = createTurtle();
+//   let radius = SUNRAD + 5;
+//   let rayEnd = pointCoord(angle, radius, 0, 0);
+//   console.log(rayEnd)
+
+//   ray.jump(rayEnd)
+
+//   while ((rayEnd[0] > 0 && rayEnd[0] < 125) && (rayEnd[1] < 125)) {
+//     radius += randIntInRange(6, 25);
+//     if (draw) {
+//       ray.goTo(pointCoord(angle, radius, 0, 0));
+//     }
+//     else {
+//       ray.jump(pointCoord(angle, radius, 0, 0));
+//     }
+
+//     draw = !draw;
+//   }
+
+//   sun.join(ray)
+// }
 
 // buildings.resample(0.01);
 
 // for (let fn of sdfParams) {  // SDF implementation for the sun path, only uncomment once sun function is done
 //   sun.iteratePath( 
 //     (pt, tValue) => {
-//       let x = sdfParams[0];
-//       let y = sdfParams[1];
-//       let width = sdfParams[2];
-//       let height = sdfParams[3];
+//       let x = fn[0];
+//       let y = fn[1];
+//       let width = fn[2];
+//       let height = fn[3];
 
 //       if (SDFRect(pt, x, y, width, height) == 0) {
 //         return "REMOVE";
@@ -198,7 +262,37 @@ for (let i = 0; i < 2; i++) {
 //   )
 // }
 
-drawTurtles([buildings]);
+// wave gen
 
+let waves = createTurtle();
+const waveNum = 39;
+const waveSpacing = HORIZON / waveNum;
 
-// drawTurtles([build1]);
+for (let i = 0; i < waveNum; i++) {
+  let tempWave = createTurtle();
+
+  tempWave.jump([0, HORIZON - (i * waveSpacing)]);
+  tempWave.forward(125);
+  
+  waves.join(tempWave);
+}
+  
+waves.resample(2);
+
+waves.iteratePath(pt => {
+  let [x, y] = pt;
+  let scale = (HORIZON - y) * 0.5
+  y += randInRange(0,1)
+  y += noise([x * 0.05]) * scale
+  y *= 2
+  y -= 40
+  
+
+  if (y < 0 || y > HORIZON) {
+    return "REMOVE";
+  }
+  
+  return [x, y];
+})
+
+drawTurtles([buildings, sun, waves]);
